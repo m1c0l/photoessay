5471e38886703667ba5a83257f09e0a2e35e8f2d
m1c0l <michael.li11702@gmail.com>
2016-06-21 13:18:50 -0400

    Add variables for common paths used

--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,23 @@ var paths = {
 	build: "./app/build"
 };
 
+// Some common file paths needed
+
+// index.jade is convenient since we need to build to index.html
+// because BrowserSync loads the build directory, so it looks for index.html
+var jadeFilename = paths.app + '/index.jade';
+var cssFilename = paths.app + '/style.css';
+var dataFilename = paths.app + '/data.json';
+var jsFilesPath = paths.app + '/*.js';
+var imgSrcPath = paths.app + '/images/*.JPG';
+var imgBuildDir = paths.build + '/images';
+
 // Jade task: Pull in image data from the json file and 
 // compile the Jade file into html, and copy to the build folder
 gulp.task('jade', function() {
-	var stream = gulp.src(paths.app + '/index.jade')
+	var stream = gulp.src(jadeFilename)
 		.pipe(data(function(file) {
-			return require(paths.app + '/data.json');
+			return require(dataFilename);
 		}))
 		.pipe(jade())
 		.pipe(gulp.dest(paths.build))
@@ -32,14 +43,14 @@ gulp.task('jade', function() {
 	// watch task to get the new file,
 	// we get the original cached file instead
 	stream.on('end', function() {
-		delete require.cache[require.resolve(paths.app + '/data.json')];
+		delete require.cache[require.resolve(dataFilename)];
 	});
 	return stream;
 });
 
 // css task: copy stylesheets to the build folder and reload the browser to show changes
 gulp.task('css', function() {
-	return gulp.src(paths.app + '/style.css')
+	return gulp.src(cssFilename)
 		.pipe(gulp.dest(paths.build))
 		.pipe(reload({stream: true}));
 });
@@ -47,7 +58,7 @@ gulp.task('css', function() {
 // scripts task: run JS scripts through JSHint and copy to build folder, reload browser to show changes
 // JSHint ignores files listed in the .jshintignore file
 gulp.task('scripts', function() {
-	return gulp.src(paths.app + '/*.js')
+	return gulp.src(jsFilesPath)
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(gulp.dest(paths.build))
@@ -59,35 +70,35 @@ gulp.task('scripts', function() {
 // To save time, only process images that have been changed.
 var resizedImageWidth = 1366;
 gulp.task('gm', function() {
-	return gulp.src(paths.app + '/images/*.JPG')
-		.pipe(changed(paths.build + '/images'))
+	return gulp.src(imgSrcPath)
+		.pipe(changed(imgBuildDir))
 		.pipe(gm(function(gmfile) {
 			return gmfile.resize(resizedImageWidth);
 		}))
-		.pipe(gulp.dest(paths.build + '/images'))
+		.pipe(gulp.dest(imgBuildDir))
 		.pipe(reload({stream: true}));
 });
 
 // Watch task: whenever a file is changed, run its corresponding task
 gulp.task('watch', function() {
-	watch(paths.app + '/style.css', function() {
+	watch(cssFilename, function() {
 		gulp.start('css');
 	});
-	watch([paths.app + '/index.jade', paths.app + '/data.json'], function() {
+	watch([jadeFilename, dataFilename], function() {
 		gulp.start('jade');
 	});
-	watch(paths.app + '/*.js', function() {
+	watch(jsFilesPath, function() {
 		gulp.start('scripts');
 	});
 	// if an image in the folder was added or modified, run the gm task
-	watch(paths.app + '/images/*.JPG', {events: ['add', 'change']}, function(file) {
+	watch(imgSrcPath, {events: ['add', 'change']}, function(file) {
 		console.log(file.basename + ' added or changed!');
 		gulp.start('gm');
 	});
 	// if an image in the folder was deleted, also delete the image with same filename in the build folder
-	watch(paths.app + '/images/*.JPG', {events: 'unlink'}, function(file) {
+	watch(imgSrcPath, {events: 'unlink'}, function(file) {
 		console.log(file.basename + ' deleted!');
-		rimraf(paths.build + '/images/' + file.basename, function(err) {
+		rimraf(imgBuildDir + '/' + file.basename, function(err) {
 			if (err) {
 				console.log('rimraf error', err);
 			}
